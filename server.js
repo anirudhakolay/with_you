e9793e5a3352f1f4f2a31d48e08b199bdcfe836d
anirudha <anirudhakolay08062001@gmail.com>
2024-04-17 18:33:57 +0530

    djfhjdk

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 
 app.get("/", (req, res) => {
-	res.send("server connected");
+	res.send({status:"started"});
 });
 
 server.listen(PORT, () => {
