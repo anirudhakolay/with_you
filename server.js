7dece170452c2b07499447a2210d7272652c3c06
anirudha <anirudhakolay08062001@gmail.com>
2024-04-17 18:30:00 +0530

    Hellooeoe

--- server.js
+++ server.js
@@ -25,8 +25,9 @@ app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+
+app.get("/", (req, res) => {
+	res.send("server connected");
 });
 
 server.listen(PORT, () => {
